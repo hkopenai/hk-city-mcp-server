6287df3e14e2f48ac6e008db49a89656bedaf6b5
Neo Alienson <neo@01man.com>
2025-07-13 19:33:39 +0800

    pylance fix

--- hkopenai/hk_city_mcp_server/server.py
+++ hkopenai/hk_city_mcp_server/server.py
@@ -18,7 +18,7 @@ def create_mcp_server():
     return mcp
 
 
-def main(args):
+def main(host: str, port: int, sse: bool):
     """
     Main function to run the MCP Server.
 
@@ -27,8 +27,8 @@ def main(args):
     """
     server = create_mcp_server()
 
-    if args.sse:
-        server.run(transport="streamable-http", host=args.host, port=args.port)
+    if sse:
+        server.run(transport="streamable-http", host=host, port=port)
         print(
             f"MCP Server started in SSE mode on port {args.port}, bound to {args.host}"
         )
